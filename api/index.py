fa33dc73d82170e526f9327e6f1b1be1d89da9f8
millet650337 <millet650337@gmail.com>
2026-04-10 22:00:35 +0800

    新增

--- api/index.py
+++ api/index.py
@@ -74,6 +74,15 @@ class SurveyData(BaseModel):
     target_gender: str = ""
     target_height: str = ""
     target_age_diff: str = ""
+    
+    # ----- 新增的問卷欄位開始 -----
+    target_future_status: List[str] = []
+    target_smoking: str = ""
+    target_drinking: str = ""
+    target_tattoo: str = ""
+    lunch_budget: str = ""
+    # ----- 新增的問卷欄位結束 -----
+    
     dealbreakers: List[str] = []
     money_view: str = ""
     gift_view: str = ""
